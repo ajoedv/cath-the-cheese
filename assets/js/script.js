29189cd0795e1a70f97158f98ec3f5110ce54103
Amjad <mamered@gmail.com>
2023-04-19 04:00:09 +0000

    fix error yscore, mscore undefined

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 const startBtn = document.getElementById("start_btn");
 const startScreen = document.getElementsByClassName("start")[0];
 const gameScreen = document.getElementsByClassName("playing")[0];
+let yscore = document.getElementById("yscore");
+let mscore = document.getElementById("mscore");
 
 // clice event to hide start stage and display playing stage
 startBtn.addEventListener('click', () => {
